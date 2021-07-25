285431a398a10606866a69a2436b2292142aac03
Genmare <gregfi@aliceadsl.fr>
2021-07-25 18:08:28 +0200

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -19,7 +19,7 @@ import { setContext } from 'apollo-link-context';
 // });
 
 const httpLink = new HttpLink({
-    uri: 'http://localhost:5000',
+    uri: 'https://polar-shore-62304.herokuapp.com/',
 });
 
 const authLink = setContext(() => {
